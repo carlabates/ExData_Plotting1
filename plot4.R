20e7e7e496b2926efa4d38fee17711fe41081047
carlabates <carlabates@users.noreply.github.com>
2015-02-11 14:29:02 -0600

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -13,5 +13,5 @@ with(subset(epcX), points(daytime, Sub_metering_3, type="l", ylim=c(0,30), col="
 with(subset(epcX), points(daytime, Sub_metering_2, type="l", ylim=c(0,30), col="red", lwd=".5"))
 legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex=0.8, lty=c(1,1,1), col=c("black","blue","red"))
 with(epcX, plot(daytime, Global_reactive_power, type="l", xlab="datetime"))
-dev.copy(png, file = "plot5.png", width=480, height=480, res=55) 
+dev.copy(png, file = "plot4.png", width=480, height=480, res=55) 
 dev.off()
